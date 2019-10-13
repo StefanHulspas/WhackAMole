17d25ad7ac97f8e7bc9cd36be225a83a50704874
Stefan <stefanhulspas@gmail.com>
2019-10-13 13:18:12 +0200

    Setup basic whack-a-mole game without score

---
+++ Assets/MoleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class MoleController : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _background;
+	[SerializeField]
+	private float moleSizePercent = .9f;
+
+	private bool isActive = false;
+	private float timeoutTime;
+	private Action<MoleController> timeoutCallback;
+
+	private void Update()
+	{
+		if (!isActive) return;
+		if (Time.timeSinceLevelLoad >= timeoutTime) {
+			PopDownMole();
+			timeoutCallback(this);
+		}
+	}
+
+	public void Setup(float size) {
+		_background.localScale = new Vector3(size * moleSizePercent, .1f, size * moleSizePercent);
+		_background.gameObject.SetActive(false);
+	}
+
+	public void PopUpMole(float baseMoleActiveTime, Action<MoleController> moleTimeout) {
+		timeoutTime = Time.timeSinceLevelLoad + baseMoleActiveTime;
+		timeoutCallback = moleTimeout;
+		_background.gameObject.SetActive(true);
+	}
+
+	public void PopDownMole() {
+		isActive = false;
+		_background.gameObject.SetActive(false);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,13 +7,21 @@ public class GameController : MonoBehaviour
 	[SerializeField]
 	private Vector2 _gridSize = new Vector2(2, 2);
 	[SerializeField]
-	private float moleBorderPercent = .1f;
+	private Vector2 _FieldAnchorMin = new Vector2(.05f, .05f);
+	[SerializeField]
+	private Vector2 _FieldAnchorMax= new Vector2(.95f, .8f);
 
 	[Header("Game Settings")]
 	[SerializeField]
-	private float playtimeInSeconds = 60f;
+	private float _playtimeInSeconds = 60f;
 	[SerializeField]
-	private GameObject _molePrefab;
+	private float _baseTimeBetweenMoles = 2f;
+	[SerializeField]
+	private float _baseMoleActiveTime = 2f;
+	[SerializeField]
+	private MoleController _molePrefab;
+	[SerializeField]
+	private LayerMask _moleLayerMask;
 
 
 	[Header("Game Variables In Scene")]
@@ -23,17 +31,22 @@ public class GameController : MonoBehaviour
 	private Vector3 _screenBotLeft;
 	private Vector3 _screenTopRight;
 
-	private List<GameObject> _inactiveMoles = new List<GameObject>();
+	private List<MoleController> _inactiveMoles = new List<MoleController>();
+	private List<MoleController> _activeMoles = new List<MoleController>();
+
+	private float nextMoleSpawn;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		if (_mainCamera == null) _mainCamera = Camera.main;
 
-		_screenBotLeft = _mainCamera.ScreenToWorldPoint(new Vector3(0, 0, _mainCamera.transform.position.y));
-		_screenTopRight = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, _mainCamera.transform.position.y));
+		_screenBotLeft = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width * _FieldAnchorMin.x, Screen.height * _FieldAnchorMin.y, _mainCamera.transform.position.y));
+		_screenTopRight = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width * _FieldAnchorMax.x, Screen.height * _FieldAnchorMax.y, _mainCamera.transform.position.y));
 
 		PreCreateMoles();
+
+		nextMoleSpawn = _playtimeInSeconds - _baseTimeBetweenMoles;
 	}
 
 	private void PreCreateMoles()
@@ -51,13 +64,79 @@ public class GameController : MonoBehaviour
 		for (int x = 0; x < _gridSize.x; x++) {
 			for (int y = 0; y < _gridSize.y; y++) {
 				Vector3 position = new Vector3(botMid.x - moleSize * _gridSize.x / 2 + moleSize / 2 + x * moleSize, _screenBotLeft.y, _screenBotLeft.z + moleSize / 2 + y * moleSize);
-				GameObject newMole = Instantiate(_molePrefab, position, Quaternion.identity, transform);
-				newMole.transform.localScale = new Vector3(moleSize, .1f, moleSize) * (1f - moleBorderPercent);
+				MoleController newMole = Instantiate(_molePrefab, position, Quaternion.identity, transform);
+				newMole.Setup(moleSize);
 				_inactiveMoles.Add(newMole);
 			}
 		}
-
 	}
 
+	private void Update()
+	{
+		HandleInput();
+		HandleGameLogic();
+	}
 
+	private void HandleInput()
+	{
+		HandleTouch();
+		HandleMouseClick();
+	}
+
+	private void HandleMouseClick()
+	{
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit raycastHit;
+			if (Physics.Raycast(ray, out raycastHit, 1000, _moleLayerMask))
+			{
+				MoleController moleController = raycastHit.collider.transform.GetComponentInParent<MoleController>();
+				moleController.PopDownMole();
+				_inactiveMoles.Add(moleController);
+			}
+		}
+	}
+
+	private void HandleTouch()
+	{
+		Touch[] touches = Input.touches;
+		for (int i = 0; i < touches.Length; i++)
+		{
+			if (touches[i].phase == TouchPhase.Began)
+			{
+				Ray ray = _mainCamera.ScreenPointToRay(touches[i].position);
+				RaycastHit raycastHit;
+				if (Physics.Raycast(ray, out raycastHit, 1000, _moleLayerMask))
+				{
+					MoleController moleController = raycastHit.collider.transform.GetComponentInParent<MoleController>();
+					moleController.PopDownMole();
+					_inactiveMoles.Add(moleController);
+				}
+			}
+		}
+	}
+
+	private void HandleGameLogic()
+	{
+		_playtimeInSeconds -= Time.deltaTime;
+		if (_playtimeInSeconds <= 0) return;
+		else if (_playtimeInSeconds <= nextMoleSpawn) {
+			SpawnNewMole();
+			nextMoleSpawn = _playtimeInSeconds - _baseTimeBetweenMoles;
+		}
+	}
+
+	private void SpawnNewMole()
+	{
+		if (_inactiveMoles.Count == 0) return;
+		MoleController MoleToSpawn = _inactiveMoles[Random.Range(0, _inactiveMoles.Count - 1)];
+		MoleToSpawn.PopUpMole(_baseMoleActiveTime, MoleTimeout);
+		_inactiveMoles.Remove(MoleToSpawn);
+		_activeMoles.Add(MoleToSpawn);
+	}
+
+	private void MoleTimeout(MoleController timedoutMole) {
+		_activeMoles.Remove(timedoutMole);
+		_inactiveMoles.Add(timedoutMole);
+	}
 }
